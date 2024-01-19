1dee66d49164b15a6b66d67f71bfd6204f01cac2
SantaKlaus <dsfdsfvdsvf@mail.ru>
2024-01-19 16:00:28 +0400

    example homeWork_01

--- homeWork_01.js
+++ homeWork_01.js
@@ -1,9 +1,12 @@
 
 const res = "B" + "a" + (1 - "hello");
-console.log(res); //BaNaN
+console.log(res);
+//BaNaN
 
 const res2 = (true && 3) + "d";
-console.log(res2); //trued
+console.log(res2);
+//3d
 
 const res3 = Boolean(true && 3) + "d";
-console.log(res3); // 
+console.log(res3);
+// trued
