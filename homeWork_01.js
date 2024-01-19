01d3b99385d3a665e2a136cd0a2e3aab7bc9e059
SantaKlaus <dsfdsfvdsvf@mail.ru>
2024-01-19 15:39:00 +0400

    firs commit homeWork01

--- homeWork_01.js
+++ homeWork_01.js
@@ -1,1 +1,9 @@
-wafaw
+
+const res = "B" + "a" + (1 - "hello");
+console.log(res); //BaNaN
+
+const res2 = (true && 3) + "d";
+console.log(res2); //trued
+
+const res3 = Boolean(true && 3) + "d";
+console.log(res3); // 
